f65b2e504dcef1de3ad2502769e4b2e4c7559f02
Ashish <ashish@MacBook-Pro.local>
2022-06-09 17:36:03 +0530

    Initial Project setup

---
+++ PrintLib/PrintLib.h
@@ -0,0 +1,18 @@
+//
+//  PrintLib.h
+//  PrintLib
+//
+//  Created by Ashish on 09/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrintLib.
+FOUNDATION_EXPORT double PrintLibVersionNumber;
+
+//! Project version string for PrintLib.
+FOUNDATION_EXPORT const unsigned char PrintLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrintLib/PublicHeader.h>
+
+
