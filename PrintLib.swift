458d2fe0aed017a7f7a639bc48bb5d6e6c5e4947
Ashish <ashish@MacBook-Pro.local>
2022-06-09 19:08:03 +0530

    add test cases

---
+++ PrintLib.swift
@@ -0,0 +1,23 @@
+//
+//  PrintLib.swift
+//  PrintLib
+//
+//  Created by Ashish on 09/06/22.
+//
+
+import Foundation
+
+
+public final class PrintLib {
+
+    let name = "PrintLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
